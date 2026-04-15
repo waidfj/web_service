c60b0d0862bc6c459d5db0003ab3e443aca35062
waidfj <wiad.f.aljamal@gmail.com>
2026-04-16 01:17:38 +0300

    structure

---
+++ server.py
@@ -0,0 +1,4 @@
+from src.main import run_server
+
+if __name__ == "__main__":
+	run_server()
---
+++ src/app/infrastructure/router.py
@@ -0,0 +1,23 @@
+def route(request_line, headers):
+	# validate if bad request
+        # return bad request
+
+    # extract the file name
+
+    # if type is document
+        # get last-modified for the file
+        # handle 'if-modified' header
+        # return the file
+        # if the file not found, return 404 not found
+
+
+    # if type is image
+        # get last-modified for the file
+        # handle 'if-modified' header
+        # return the file
+        # if the file not found, return 404 not found
+
+
+    # if another type
+        # return bad request
+    pass
