e925ad0b2d40b00ffa2b9f49a286c414a32f9fd4
waidfj <wiad.f.aljamal@gmail.com>
2026-04-16 17:04:42 +0300

    generate forbidden response

--- src/app/infrastructure/router.py
+++ src/app/infrastructure/router.py
@@ -1,3 +1,4 @@
+from src.app.application.generate_forbidden import generate_forbidden
 from src.app.application.generate_not_modified import generate_not_modified
 from src.app.application.generate_not_found import generate_not_found
 from src.app.application.generate_bad_request import generate_bad_request
@@ -5,7 +6,7 @@ from src.app.application.generate_ok import generate_ok
 from src.app.consts.file_types import FILE_TYPES
 from src.app.consts.header_names import CONNECTION, CONNECTION_HEADER, \
     IF_MODIFIED_SINCE_HEADER, SEC_FETCH_DEST_HEADER
-from src.app.consts.server_env import FILES_PATH, HOME
+from src.app.consts.server_env import FILES_PATH, HOME, PRIVATE_FOLDER
 from src.app.infrastructure.generate_content import generate_content
 from src.app.infrastructure.get_last_modified import get_last_modified
 
@@ -25,6 +26,9 @@ def route(request_line, headers):
     # extract the file name
     filename = HOME if request_line[1] == '/' else request_line[1]
     filepath = FILES_PATH + filename
+
+    if PRIVATE_FOLDER in filepath.split('/'):
+        return generate_forbidden(connection)
 
     try :
         last_modified = get_last_modified(filepath)
